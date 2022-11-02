b8387fdaa2a9ecc496d0f887e6f10af48c9a6438
Allan <allan.kimurgor@student.moringaschool.com>
2022-11-02 13:51:00 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 24, "M", "Brown", "Choleric", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 20, "M", "Dark", "Sanguine", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 19, "F", "Black", "Melancholic", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 32, "M", "White", "Phlegmatic", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 17, "F", "White", "Sanguine", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 25, "M", "Purple", "Phlegmatic", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 22, "F", "Chocolate", "Sanguine", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 21, "M", "Yellow", "Choleric", False);
